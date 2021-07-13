c654af81ac8d66e9bf002890a5dbf6e3d3399785
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-13 07:52:14 +0700

    Initial commit

---
+++ lib/App/DiffPDFText.pm
@@ -0,0 +1,15 @@
+package App::DiffDocText;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Diff the text of two Office word-processor documents (.doc, .docx, .odt, etc)
+
+=head1 SYNOPSIS
+
+See the included script L<diff-doc-text>.
+
+=cut
